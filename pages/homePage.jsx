21f0adb33e6c1400604170768c9b9bfe3627b50f
Hicham Thabti <thabti.hicham@gmail.com>
2024-01-07 21:39:14 +0100

    css changes

--- pages/homePage.jsx
+++ pages/homePage.jsx
@@ -90,6 +90,7 @@ const HomePage = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const bgColor = isDarkMode ? "bg-darkblue" : "bg-white";
   const textColor = isDarkMode ? "text-white" : "text-darkblue";
+  const bgColor2 = isDarkMode ? "bg-midblue" : "bg-white";
 
   // modal handling
 
@@ -265,83 +266,86 @@ const HomePage = () => {
       </div>
       {/* filter area end */}
       {/* jobs listing area start */}
-      {isLoading ? <Spinner textColor={textColor} /> :
-      <div className="grid grid-cols-1 sm:grid-cols-3 gap-8">
-        {jobsDisplayed.map((job) => (
-          <div
-            key={job.id}
-            onClick={() => openModal(job)}
-            className={`p-4 ${bgColor} m-12 w-72 h-60 flex cursor-pointer flex-col justify-between rounded-xl transition-all duration-300 transform hover:scale-105 ${
-              isDarkMode
-                ? "shadow-light hover:shadow-hover-light"
-                : "shadow-dark hover:shadow-hover-dark"
-            }`}
-          >
-            <div className="flex flex-col items-center justify-center">
-              <h1
-                className={`${textColor} text-xl font-quick text-center font-bold mb-8`}
-              >
-                {job.jobTitle}
-              </h1>
-
-              <div className="flex items-center justify-center text-center mb-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className={`w-4 h-4 mr-2 ${textColor}`}
+      {isLoading ? (
+        <Spinner textColor={textColor} />
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-8">
+          {jobsDisplayed.map((job) => (
+            <div
+              key={job.id}
+              onClick={() => openModal(job)}
+              className={`p-4 ${bgColor} m-12 w-72 h-60 flex cursor-pointer flex-col justify-between rounded-xl transition-all duration-300 transform hover:scale-105 ${
+                isDarkMode
+                  ? "shadow-light hover:shadow-hover-light"
+                  : "shadow-dark hover:shadow-hover-dark"
+              } 
+  `}
+            >
+              <div className="flex flex-col items-center justify-center">
+                <h1
+                  className={`${textColor} text-xl font-quick text-center font-bold mb-8`}
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
-                  />
-                </svg>
-                <h1 className={`${textColor} font-quick font-bold text-md`}>
-                  {job.companyName}
+                  {job.jobTitle}
                 </h1>
+
+                <div className="flex items-center justify-center text-center mb-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className={`w-4 h-4 mr-2 ${textColor}`}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
+                    />
+                  </svg>
+                  <h1 className={`${textColor} font-quick font-bold text-md`}>
+                    {job.companyName}
+                  </h1>
+                </div>
+
+                <div className="flex items-center justify-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className={`w-4 h-4 mr-2 ${textColor}`}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                    />
+                  </svg>
+                  <p className={`${textColor} font-quick font-normal`}>
+                    {job.location}
+                  </p>
+                </div>
               </div>
 
-              <div className="flex items-center justify-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className={`w-4 h-4 mr-2 ${textColor}`}
+              {/* Card Footer */}
+              <div className="w-full pt-2 border-t border-gray-300">
+                <p
+                  className={`${textColor} font-quick font-normal text-sm text-center`}
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
-                  />
-                </svg>
-                <p className={`${textColor} font-quick font-normal`}>
-                  {job.location}
+                  Open for details
                 </p>
               </div>
             </div>
-
-            {/* Card Footer */}
-            <div className="w-full pt-2 border-t border-gray-300">
-              <p
-                className={`${textColor} font-quick font-normal text-sm text-center`}
-              >
-                Open for details
-              </p>
-            </div>
-          </div>
-        ))}
-      </div>
-      }
+          ))}
+        </div>
+      )}
       <ReactPaginate
         className={`${textColor} font-quick font-bold flex mb-8 justify-center`}
         previousLabel={"← Previous"}
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
       },
       colors: {
         darkblue: "#15253e",
+        darkblue2: "#294a7d",
         midblue: "#009bc4",
         lightblue: "#18b8d4",
         filterblue: "#009cc5",
@@ -21,9 +22,11 @@ module.exports = {
       boxShadow: {
         dark: "rgba(6, 24, 44, 0.1) 0px 0px 0px 2px, rgba(6, 24, 44, 0.4) 0px 2px 4px -1px, rgba(255, 255, 255, 0.05) 0px 1px 0px inset",
         light:
-          "rgba(255, 255, 255, 0.05) 0px 0px 0px 2px, rgba(255, 255, 255, 0.4) 0px 2px 4px -1px, rgba(0, 0, 0, 0.05) 0px 1px 0px inset",
-        "hover-dark": "rgba(0, 0, 0, 0.35) 0px 12px 30px 2px",
-        "hover-light": "rgba(255, 255, 255, 0.15) 0px 12px 30px 2px",
+          "rgba(255, 255, 255, 0.4) 0px 0px 0px 1px, rgba(255, 255, 255, 0.4) 0px 1px 2px -1px, rgba(0, 0, 0, 0.05) 0px 1px 0px inset",
+
+        "hover-dark": "rgba(0, 0, 0, 0.2) 0px 12px 30px 2px",
+        "hover-light":
+          "rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.7) 0px -3px 0px inset",
       },
       fontFamily: {
         quick: ["Quicksand", "sans-serif"],
